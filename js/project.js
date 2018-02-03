1c1003a132bf783f98af311ea9d4f31ba7d8ed4f
Saman Rashidi <Saman@edoramedia.com>
2018-02-03 13:04:00 +0330

    top-header finished

--- js/project.js
+++ js/project.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
-// All JS for website goes here
+// Search Overlay
+
+$('.search input').on('focus', function(){
+	$('.search-overlay').addClass('active');
+}).on('blur', function(){
+	$('.search-overlay').removeClass('active');
+});
 
 });
